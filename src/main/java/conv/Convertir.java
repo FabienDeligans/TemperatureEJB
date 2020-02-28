66c2da9c760a92856e4c297fab75e19c18019810
Admin <Admin@DESKTOP-GMA9Q23.localdomain>
2020-02-28 11:10:46 +0100

    premier commit

---
+++ src/main/java/conv/Convertir.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conv;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Admin
+ */
+@Stateless
+public class Convertir {
+
+    public double ConvertirCelciusToFahrenheit(double t){
+        double fahrenheit; 
+        fahrenheit = 9.0 / 5.0 * t + 32; 
+        return fahrenheit; 
+    }
+    
+    public double convertirFahrenheitToCelcius(double t){
+        double celcius; 
+        celcius = (t-32) * 5.0 / 9.0; 
+        return celcius; 
+    }
+}
